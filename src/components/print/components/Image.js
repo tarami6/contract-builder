dea906988aedb96b43678fcc5dc04ad973d828ee
tarami6 <rami.timefreek@gmail.com>
2021-03-01 10:36:41 +0200

    print img fixed

--- src/components/print/components/Image.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Image = () => {
-    return <img src={'https://www.amdocs.com/sites/default/files/inline-images/optima-logo-sm-whiteBG.jpg'}
-                alt="Logo"/>;
-}
-export default Image
